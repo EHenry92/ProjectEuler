766f919b8d568d0608c0655fb9f2d19e82bbb8e4
EHenry92 <EHenry692@hotmail.com>
2018-03-13 23:52:56 -0400

    Problem 9

--- js/6-sum_square_difference.js
+++ js/6-sum_square_difference.js
@@ -1,12 +1,7 @@
 /*
-The sum of the squares of the first ten natural numbers is,
-1^2 + 2^2 + ... + 10^2 = 385
-
-The square of the sum of the first ten natural numbers is,
-(1 + 2 + ... + 10)^2 = 552 = 3025
-
+The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... + 10^2 = 385
+The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 552 = 3025
 Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
-
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 */
 
@@ -22,24 +17,3 @@ const sumSquareDifference = upperBound => {
 console.log(sumSquareDifference(10));
 console.log(sumSquareDifference(100));
 
-
-
-
-
-// const sumOfSquares = upperBound => {
-//   let total = 1;
-//   for (let i = 2; i<= upperBound; i++) {
-//     total += i * i;
-//   }
-//   return total;
-// }
-
-// const squareOfSum = upperBound => {
-//     sum = 1;
-//     for(let i = 2; i<=upperBound; i++) {
-//       sum += i;
-//     }
-//     return sum * sum;
-// }
-
-// const sumSquareDifference = upper => squareOfSum(upper) - sumOfSquares(upper);
---
+++ js/9-special_pythagorean_triplet.js
@@ -0,0 +1,39 @@
+/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
+
+const isPyTriplet = (a, b, c) =>
+  Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2);
+
+/*
+a + b + c = y ----> c = y - a - b
+a^2 + b^2 = c^2 ----> a^2 + b^2 = (y - a - b)^2
+  ...
+  0 = y^2 - 2ya - 2yb + 2ab
+  ...
+  -(y^2)/2 = ab - ya - yb
+  ...
+  [(y^2) /2 ] + ya = b(a - y)
+  ...
+    ([-(y^2) /2 ] + ya )/ (a-y)= b
+*/
+
+const specialPythagoreanTriplet = (sum) => {
+  for (let a = 1; a < sum / 3; a++) {
+    let b = (((0 - Math.pow(sum, 2)) / 2) + (sum * a)) / (a - sum);
+    if (b % 1 === 0) {
+      let c = sum - a - b;
+      if (isPyTriplet(a, b, c)) {return (a * b * c);}
+    }
+  }
+  return false;
+};
+
+
+console.log(specialPythagoreanTriplet(12));
+console.log(specialPythagoreanTriplet(30));
+console.log(specialPythagoreanTriplet(1000));
